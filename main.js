8b4d3dc72f0cd592cfddb66ad70318130a95eb5c
mykolassl <emykolas@gmail.com>
2022-04-30 21:02:41 +0300

    Add sketch functionality on grid hover

--- main.js
+++ main.js
@@ -4,5 +4,8 @@ grid.style.cssText = `grid-template-columns: repeat(${cols}, 1fr)`
 
 for(let i = 0; i < cols**2; i++) {
     let div = document.createElement('div');
+    div.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = 'black';
+    })
     grid.appendChild(div);
 }
