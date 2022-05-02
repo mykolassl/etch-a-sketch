473419bad517b7a041105bd52279500ec0ba516a
mykolassl <emykolas@gmail.com>
2022-05-02 20:38:06 +0300

    Fix grid item streching issue on grid size change

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
 const grid = document.querySelector('#grid');
-let cols = prompt('How many columns?')
-grid.style.cssText = `grid-template-columns: repeat(${cols}, 1fr)`
+const gridsizeInput = document.querySelector('#gridsize');
+const gridsizeValue = document.querySelector('#gridsize-value');
+const colorWheel = document.querySelector('#color-wheel');
 
-for(let i = 0; i < cols**2; i++) {
-    let div = document.createElement('div');
-    div.addEventListener('mouseover', (e) => {
-        e.target.style.backgroundColor = 'black';
-    })
-    grid.appendChild(div);
+// Change label text value based on what is selected on range input.
+gridsizeInput.addEventListener('input', (e) => {
+    gridsizeValue.textContent = `${e.target.value}x${e.target.value}`;
+});
+
+// When user releases range input, removes every child element from the grid, and sets up the grid
+// columns to the value that was selected in range input.
+gridsizeInput.addEventListener('change', (e) => {
+    grid.replaceChildren();
+    setupGrid(e.target.value);
+})
+
+function setupGrid(cols) {
+    grid.style.cssText = `grid-template-columns: repeat(${cols}, 1fr)`
+
+    for(let i = 0; i < cols**2; i++) {
+        let div = document.createElement('div');
+
+        div.addEventListener('mouseover', (e) => {
+            e.target.style.backgroundColor = 'black';
+        });
+        
+        grid.appendChild(div);
+    }
 }
+
+setupGrid(10)
